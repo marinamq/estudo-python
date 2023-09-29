b35c1ec86d0e4bed9beb7481cc538a0b5be8a289
Marina Milhomens de Queiroz <109256347+marinamq@users.noreply.github.com>
2023-09-29 21:03:15 +0000

    adicionada as aulas

---
+++ estudo02.py
@@ -0,0 +1,2 @@
+nome = input('Qual o seu nome? ')
+print('Boas vindas! Prazer em te conhecer {}!'.format(nome))
---
+++ estudo03.py
@@ -0,0 +1,4 @@
+# Desafio 003
+x = int(input('Digite o primeiro número: '))
+y = int(input('Digite o segundo número: '))
+print('A soma é {}.'.format(x+y))
